af510d6d07ad7c6d1547cdc06a3a4703a8e50668
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-04-13 19:44:47 +0600

    logged user info display in ui and signout added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 
+
 const App = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center h-screen'>
       <Header/>
       <Outlet/>
     </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import React, { useState } from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import app from '../firebase/firebase.init';
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
-    const handleGoogleSignIn = () =>{
-       signInWithPopup(auth, provider)
-       .then(result => {
-        console.log(result.user)
-       })
-       .catch(error => {
-        console.log('errror' , error.message)
-       })
-        }
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedUser = result.user
+                setUser(loggedUser)
+                console.log(loggedUser)
+            })
+            .catch(error => {
+                console.log('errror', error.message)
+            })
+    }
+    const handleLogOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result)
+                setUser(null)
+            })
+            .then(error => {
+                console.log(error)
+            })
+    }
     return (
-        <div>
-          <button onClick={()=>handleGoogleSignIn()}>Google Login</button>
+        <div className='mt-5'>
+            {user ?
+                <button className='btn btn-error ml-5' onClick={() => handleLogOut()}>Logout</button>
+                : <button className='btn btn-primary' onClick={() => handleGoogleSignIn()}>Login</button>}
+
+            {user && <div>
+                <h3>{user?.displayName}</h3>
+                <img src={user.photoURL} alt="user photo" />
+            </div>}
         </div>
+
     );
 };
 
