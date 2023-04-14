583b7d5d63dd4a97de71dcd640f7038e2a575441
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-04-14 14:27:06 +0600

    github login intrigation added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, GithubAuthProvider } from "firebase/auth";
 import app from '../firebase/firebase.init';
 const Login = () => {
     const [user, setUser] = useState(null)
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const loggedUser = result.user
+                setUser(loggedUser)
+                console.log(loggedUser)
+            })
+            .catch(error => {
+                console.log('errror', error.message)
+            })
+    }
+    const handleGithubLogin = () => {
+        signInWithPopup(auth, githubProvider)
             .then(result => {
                 const loggedUser = result.user
                 setUser(loggedUser)
@@ -31,7 +43,12 @@ const Login = () => {
         <div className='mt-5'>
             {user ?
                 <button className='btn btn-error ml-5' onClick={() => handleLogOut()}>Logout</button>
-                : <button className='btn btn-primary' onClick={() => handleGoogleSignIn()}>Login</button>}
+                :
+                <div className="flex gap-5">
+                    <button className='btn btn-primary' onClick={() => handleGoogleSignIn()}>Google login</button>
+                    <button className='btn btn-primary' onClick={() => handleGithubLogin()}>Github login</button>
+                </div>
+            }
 
             {user && <div>
                 <h3>{user?.displayName}</h3>
